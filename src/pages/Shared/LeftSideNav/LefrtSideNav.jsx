ffe53ce8d99f6a5171fac06a03b7d95289d496e5
shaadi1478 <sheikhshaadi137@gmail.com>
2024-04-15 13:36:35 +0600

    added categories

--- src/pages/Shared/LeftSideNav/LefrtSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LefrtSideNav.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LefrtSideNav = () => {
+    const [cateGories, setCateGories] = useState([]);
+
+    useEffect(()=>{
+        fetch('categories.json')
+        .then(ref => ref.json())
+        .then(data => setCateGories(data))
+    },[])
     return (
-        <div>
-            <h2 className="text-3xl">Left Side Nav</h2>
+        <div className="space-y-7">
+            <h2 className="text-3xl">All CateGories</h2>
+            {
+                cateGories.map(cateGories => <Link 
+                    className="block ml-4 text-xl font-semibold"
+                    key={cateGories.id}
+                    to={`/categories/${cateGories.id}`}
+                    >{cateGories.name}
+                    </Link>)
+            }
         </div>
     );
 };
